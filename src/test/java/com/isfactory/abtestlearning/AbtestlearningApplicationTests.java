470201e6a91b72719a13a1ae3ea72d5d50135536
DESKTOP-V4174FI\ointh <ointhe@gmail.com>
2022-05-06 10:59:10 +0900

    init

---
+++ src/test/java/com/isfactory/abtestlearning/AbtestlearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isfactory.abtestlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbtestlearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
