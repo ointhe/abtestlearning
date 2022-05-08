70098ca253e84008bd5b7565bf973d4795113624
DESKTOP-V4174FI\ointh <ointhe@gmail.com>
2022-05-08 19:34:06 +0900

    #TEST authorization server

---
+++ src/main/java/com/isfactory/abtestlearning/domain/oauth2/Oauth2AuthorizationConfig.java
@@ -0,0 +1,21 @@
+package com.isfactory.abtestlearning.domain.oauth2;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@Configuration
+@EnableAuthorizationServer
+public class Oauth2AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
+
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            clients.inMemory()
+                    .withClient("foo")
+                    .secret("bar")
+                    .redirectUris("http://localhost:8081/oauth2/callback")
+                    .authorizedGrantTypes("authorization_code")
+                    .scopes("read", "write")
+                    .accessTokenValiditySeconds(30000);
+        }
+    }
