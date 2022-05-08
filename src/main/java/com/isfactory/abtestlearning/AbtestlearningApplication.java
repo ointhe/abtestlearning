54cd347453726d2f5605e3c8d660aa23263d8108
DESKTOP-V4174FI\ointh <ointhe@gmail.com>
2022-05-08 19:34:06 +0900

    #TEST authorization server

--- src/main/java/com/isfactory/abtestlearning/AbtestlearningApplication.java
+++ src/main/java/com/isfactory/abtestlearning/AbtestlearningApplication.java
@@ -1,13 +1,19 @@
 package com.isfactory.abtestlearning;
 
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration;
 
 @SpringBootApplication
 public class AbtestlearningApplication {
 
 	public static void main(String[] args) {
+		String credentials = "foo:bar";
+		String encodedCredentials = new String(Base64.encodeBase64(credentials.getBytes()));
+		System.out.println(encodedCredentials);
 		SpringApplication.run(AbtestlearningApplication.class, args);
 	}
+
 
 }
